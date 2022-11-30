02856339befc9f4702055e868fbaa3ea5548f6d5
Patrick Cunningham <patrickcunningham1400@gmail.com>
2022-11-29 21:37:52 -0600

    added catch files

--- tests/tests.cc
+++ tests/tests.cc
@@ -2,7 +2,7 @@
 #  define CATCH_CONFIG_MAIN
 #endif
 
-#include <catch/catch.hpp>
+#include "catch.h"
 #include <string>
 
 #include "functions.hpp"
@@ -11,3 +11,14 @@ TEST_CASE("A test case", "[case-1]") {
   bool something = true;
   REQUIRE(something == true);
 }
+
+TEST_CASE("A test case", "[case-1]") {
+  bool something = true;
+  REQUIRE(something == true);
+}
+
+//write tests cases for, shortest path, and graph
+
+//graph, verify that the correct number of airports, correct number of unique arilines, the adjacency list is correct
+
+
