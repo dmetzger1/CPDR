78e167ec8621835bf36fcfcfb8f7d829dc626d2f
dmetzger1 <112907454+dmetzger1@users.noreply.github.com>
2022-12-11 19:02:12 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,25 @@ int main(int argc, char* argv[]) {
   cout << "Enter destination airport:";
   cin >> input2;
   vector<string> path = g->BFS(input1, input2);
-  cout << "The path with the least amount of stops from " << input1 << " to " << input2 << " using BFS is " << endl;
-  for (string airport : path) {
-        cout << airport << endl;
+  if(path.empty()){
+    cout<<"There is no path between the airports with BFS"<<endl;
+  } else{
+    cout << "The path with the least amount of stops from " << input1 << " to " << input2 << " using BFS is " << endl;
+    for (string airport : path) {
+          cout << airport << endl;
+    }
   }
 
-  double distance_ = g->dijkstras(input1, input2, argv[2]); 
-  cout<<"The shortest distance between the airports using Dijkstra's algorithm is "<<distance_<<endl;
+  pair<vector<string>, int> test_d = g->dijkstras(input1, input2, argv[2]);
+  cout<<"The shortest distance between the airports using Dijkstra's algorithm is "<<test_d.second<<endl;
+  if(test_d.first.empty()){
+    cout<<"There is no path between the airports with Dijkstras"<<endl;
+  } else{
+    cout<<"The shortest distance path between the aiports using Dijkstra's is "<<endl;
+    for(unsigned x = 0; x < test_d.first.size(); x++){
+      cout<<test_d.first[x]<<endl;
+    }
+  }
 
   std::set<string> visited;
   const clock_t begin_time = clock();
